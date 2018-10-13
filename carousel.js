68eced592832e6b4c4fc0a44bda03444266e24f4
Justin Bansal <justinbansal@gmail.com>
2018-10-13 16:00:52 -0400

    adding show function

--- carousel.js
+++ carousel.js
@@ -1,1 +1,16 @@
-var one = 1
+let count = 1;
+
+// click next button to change picture
+function next() {
+  count += 1;
+  console.log('Next button was clicked');
+  console.log(count);
+}
+
+// show image equal to count
+function show() {
+  console.log('Function show triggered...');
+  if ((document.getElementById('image1') == ('image' + count)) {
+    
+  }
+}
