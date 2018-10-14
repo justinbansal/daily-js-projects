3795abf6be812a880327c6c428baf495933e30f1
Justin Bansal <justinbansal@gmail.com>
2018-10-14 13:43:42 -0400

    replaced show function with for loop to check for match

--- carousel.js
+++ carousel.js
@@ -1,24 +1,26 @@
 let count = 1;
 
+// array of image ID's
+// loop through this looking for a match and showing the match, hide rest
+const imageIDs = ['image1', 'image2', 'image3'];
+
+function checkForMatch() {
+  for (let i = 0; i < imageIDs.length; i += 1) {
+    const image = document.getElementById(imageIDs[i]);
+    if (imageIDs[i] === (`image${count}`)) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  }
+}
+
 // click next button to change picture
 function next() {
   count += 1;
   console.log('Next button was clicked');
   console.log(count);
-  show()
+  checkForMatch();
 }
 
-// show image equal to count
-function show() {
-  console.log('Function show triggered...');
-  console.log(`image${count}`);
-  const imageID = `image${count}`;
-  const image = document.getElementById(imageID);
-  if (window.getComputedStyle(image).display === 'none') {
-    image.style.display = 'block';
-  } else {
-    image.style.display = 'none';
-  }
-}
-
-show();
+checkForMatch();
