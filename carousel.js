8923f603ec4e550c86f77aec4ac66adad806edb7
Justin Bansal <justinbansal@gmail.com>
2018-10-14 13:22:22 -0400

    added show function to show image based on current count

--- carousel.js
+++ carousel.js
@@ -5,12 +5,20 @@ function next() {
   count += 1;
   console.log('Next button was clicked');
   console.log(count);
+  show()
 }
 
 // show image equal to count
 function show() {
   console.log('Function show triggered...');
-  if ((document.getElementById('image1') == ('image' + count)) {
-    
+  console.log(`image${count}`);
+  const imageID = `image${count}`;
+  const image = document.getElementById(imageID);
+  if (window.getComputedStyle(image).display === 'none') {
+    image.style.display = 'block';
+  } else {
+    image.style.display = 'none';
   }
 }
+
+show();
