54676ec006bd2aaf1f2d9fccf8dce04d58ae10e7
Justin Bansal <justinbansal@gmail.com>
2018-12-23 14:16:07 -0500

    loop through data stored in local storage and render elements on page

--- doodle.js
+++ doodle.js
@@ -1,11 +1,15 @@
-// Form Elements
+// DOM Elements
 const titleInput = document.getElementById('title');
 const locationInput = document.getElementById('location');
 const commentsInput = document.getElementById('comments');
 const eventInput = document.getElementById('event');
 const addBtn = document.getElementById('addDate');
 const submitBtn = document.getElementById('submit');
-let pollID = 0;
+const pollRow = document.getElementById('poll-row');
+
+
+// Variables
+let polls = [];
 
 function addDate(e) {
   e.preventDefault();
@@ -15,16 +19,39 @@ function addDate(e) {
   document.getElementById('dateRow').appendChild(input);
 }
 
+// loop through array of polls and render them on the page
+function displayPolls() {
+  const storedPolls = JSON.parse(localStorage.getItem('polls'));
+  if (storedPolls) {
+    polls = storedPolls;
+  }
+  for (let i = 0; i < polls.length; i += 1) {
+    const dateHeading = document.createElement('h1');
+    pollRow.appendChild(dateHeading);
+    dateHeading.innerHTML = polls[i].date;
+    const voteCheck = document.createElement('INPUT');
+    voteCheck.type = 'checkbox';
+    voteCheck.name = 'voting';
+    pollRow.appendChild(voteCheck);
+    const submitName = document.createElement('INPUT');
+    submitName.type = 'text';
+    pollRow.appendChild(submitName);
+  }
+}
+
 function saveForm(e) {
   e.preventDefault();
   const poll = {};
-  pollID += 1;
   poll.title = titleInput.value;
   poll.location = locationInput.value;
   poll.comments = commentsInput.value;
   poll.date = eventInput.value;
-  localStorage.setItem(`poll${pollID}`, JSON.stringify(poll));
+  polls.push(poll);
+  displayPolls();
+  localStorage.setItem('polls', JSON.stringify(polls));
 }
 
 addBtn.addEventListener('click', addDate);
 submitBtn.addEventListener('click', saveForm);
+
+displayPolls();
