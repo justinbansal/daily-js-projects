6fa37982f4c0b023c3c92750074ed12b2d2bdcc3
Justin Bansal <justinbansal@gmail.com>
2018-12-27 19:10:49 -0500

    refactored date logic using queryselectorall with classname

--- doodle.js
+++ doodle.js
@@ -1,11 +1,12 @@
 // DOM Elements
 const titleInput = document.getElementById('title');
 const locationInput = document.getElementById('location');
-const dateInput1 = document.getElementById('date1');
-const dateInput2 = document.getElementById('date2');
-const dateInput3 = document.getElementById('date3');
-const dateInput4 = document.getElementById('date4');
-const dateInput5 = document.getElementById('date5');
+// these should be created within the function otherwise null at document load
+// const dateInput1 = document.getElementById('date1');
+// const dateInput2 = document.getElementById('date2');
+// const dateInput3 = document.getElementById('date3');
+// const dateInput4 = document.getElementById('date4');
+// const dateInput5 = document.getElementById('date5');
 const addBtn = document.getElementById('addDate');
 const submitBtn = document.getElementById('submit');
 const dateGroup = document.getElementById('date-group');
@@ -89,7 +90,7 @@ function addDate(e) {
   const newInput = document.createElement('input');
   newInput.type = 'date';
   newInput.name = 'date';
-  newInput.id = `date${dateID}`;
+  newInput.className = 'date';
   dateGroup.appendChild(label);
   dateGroup.appendChild(newInput);
 }
@@ -100,27 +101,17 @@ function saveForm(e) {
   const poll = {};
   poll.title = titleInput.value;
   poll.location = locationInput.value;
-  // poll.date = eventInput.value;
-  // poll.id = pollID;
-  // poll.voters = [];
-  // poll.votes = '';
   poll.eventDetails = [];
   // create new object for each date
-  for (let d = 1; d <= dateID; d += 1) {
+  const dates = document.querySelectorAll('.date');
+  for (let i = 0; i < dates.length; i += 1) {
     const event = {};
-    // event.date = dateInput1.value;
-    event.date = `dateInput${d}`.value;
+    event.date = dates[i].value;
     event.id = pollID;
     event.voters = [];
     event.votes = '';
     poll.eventDetails.push(event);
   }
-  // const event = {};
-  // event.date = dateInput1.value;
-  // event.id = pollID;
-  // event.voters = [];
-  // event.votes = '';
-  // poll.eventDetails.push(event);
   polls.push(poll);
   localStorage.setItem('polls', JSON.stringify(polls));
   // displayPolls();
