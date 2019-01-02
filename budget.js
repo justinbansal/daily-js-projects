6e930afbeb51ffdbbfc504c495f94ed74cf64429
Justin Bansal <justinbansal@gmail.com>
2019-01-02 13:28:10 -0500

    moved variables into function, otherwise value is null at time of page load

--- budget.js
+++ budget.js
@@ -1,12 +1,12 @@
 // DOM Elements
-const USERNAME = document.querySelector('input[id="username"]');
-const PASSWORD = document.querySelector('input[id="password"]');
 const REGISTER_BUTTON = document.querySelector('.registerBtn');
 
 /* REGISTER FUNCTION
  * Save username and password to local storage
 */
 function registerUser() {
+  const USERNAME = document.querySelector('input[id="username"]').value;
+  const PASSWORD = document.querySelector('input[id="password"]').value;
   localStorage.setItem('username', USERNAME);
   localStorage.setItem('password', PASSWORD);
 }
